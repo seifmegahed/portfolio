e5eb93da5c4f83fb9fd5e09514b605d380947ca1
seifmegahed <seifmegahed@me.com>
2024-06-23 05:08:01 +0300

    text input field

---
+++ src/components/InputField.tsx
@@ -0,0 +1,38 @@
+export default function InputField(props: {
+  label: string;
+  value: string;
+  onchange: (value: string) => void;
+  textField?: boolean;
+}) {
+  return (
+    <div className="w-full relative flex flex-col mb-2">
+      <div className="h-8"></div>
+      {props?.textField ? (
+        <textarea
+          value={props.value}
+          onChange={(e) => props.onchange(e.target.value)}
+          placeholder={props.label}
+          className="peer w-full p-3 focus:outline-none rounded-xl resize-none focus:placeholder-white"
+          rows={4}
+        />
+      ) : (
+        <input
+          value={props.value}
+          onChange={(e) => props.onchange(e.target.value)}
+          type="text"
+          placeholder={props.label}
+          className="peer w-full h-12 p-3 focus:outline-none outline-none rounded-xl focus:placeholder-white"
+        />
+      )}
+      <div
+        className={
+          props.value === ""
+            ? "absolute top-9 left-1 p-2 peer-focus:visible peer-focus:-translate-x-1 peer-focus:-translate-y-10 peer-focus:scale-125 invisible duration-300 transition-all ease-in-out"
+            : "absolute top-9 left-1 p-2 -translate-x-1 -translate-y-10 scale-125"
+        }
+      >
+        {props.label}
+      </div>
+    </div>
+  );
+}
--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,10 +1,27 @@
+import { useState } from "react";
 import SectionTitle from "../components/SectionTitle";
 import SectionWrapper from "../components/SectionWrapper";
+import InputField from "../components/InputField";
 
 export default function Contact() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
   return (
     <SectionWrapper id="contact">
       <SectionTitle title="Contact" />
+      <div className="w-full h-full max-w-96 flex flex-col items-center justify-center text-gray-700 gap-4">
+        <form className="w-full">
+          <InputField label="Name" value={name} onchange={setName} />
+          <InputField label="Email" value={email} onchange={setEmail} />
+          <InputField
+            label="Message"
+            value={message}
+            onchange={setMessage}
+            textField
+          />
+        </form>
+      </div>
     </SectionWrapper>
   );
 }
