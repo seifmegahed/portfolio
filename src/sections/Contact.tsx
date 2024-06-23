0eeaee72d58c261a12596d2189727d915c84b927
seifmegahed <seifmegahed@me.com>
2024-06-23 08:08:46 +0300

    hcaptcha for contact form

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -3,6 +3,7 @@ import SectionTitle from "../components/SectionTitle";
 import SectionWrapper from "../components/SectionWrapper";
 import InputField from "../components/InputField";
 import { checkEmailValidity } from "../utils/validation";
+import HCaptcha from "@hcaptcha/react-hcaptcha";
 
 const errorsInitial = {
   name: false,
@@ -14,6 +15,7 @@ export default function Contact() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
+  const [captchaToken, setCaptchaToken] = useState<string | null>(null);
   const [submitted, setSubmitted] = useState(
     sessionStorage.getItem("submitted") === "true"
   );
@@ -60,6 +62,7 @@ export default function Contact() {
         email,
         message,
         access_key: import.meta.env.VITE_PUBLIC_WEB3FORMS_API_KEY,
+        "h-captcha-response": captchaToken,
       }),
     });
     console.log(response);
@@ -72,11 +75,14 @@ export default function Contact() {
       alert("Something went wrong. Please try again later.");
     }
   };
-
+  const onHCaptchaChange = (token: string) => {
+    console.log(token);
+    setCaptchaToken(token);
+  };
   return (
     <SectionWrapper id="contact">
       <SectionTitle title="Contact" />
-      <div className="w-full h-full max-w-96 flex flex-col items-center md:justify-center text-gray-700">
+      <div className="w-full h-full max-w-screen-sm flex flex-col items-center md:justify-center text-gray-700">
         <InputField
           error={errors.name}
           label="Name"
@@ -96,23 +102,31 @@ export default function Contact() {
           onchange={setMessage}
           textField
         />
-        <div className="flex justify-end items-center w-full mt-6">
-          <button
-            className={
-              submitted
-                ? "w-32 bg-blue-100 text-white font-bold py-2 px-4 rounded"
-                : "w-32 bg-blue-500 text-white font-bold py-2 px-4 rounded"
-            }
-            onClick={handleSubmit}
-            onKeyDown={(e) => {
-              if (e.key === "Enter") {
-                handleSubmit();
+        <div className="grid h-fit w-full md:grid-cols-2 gap-4">
+          <HCaptcha
+            data-callback={() => console.log("success")}
+            sitekey="50b2fe65-b00b-4b9e-ad62-3ba471098be2"
+            reCaptchaCompat={false}
+            onVerify={onHCaptchaChange}
+          />
+          <div className="flex justify-end h-full items-center w-full">
+            <button
+              className={
+                submitted
+                  ? "w-32 bg-slate-100 text-white font-bold py-2 px-4 rounded"
+                  : "w-32 bg-slate-700 text-white font-bold py-2 px-4 rounded"
               }
-            }}
-            // disabled={submitted}
-          >
-            {submitted ? "Sent" : "Send"}
-          </button>
+              onClick={handleSubmit}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  handleSubmit();
+                }
+              }}
+              disabled={submitted || !captchaToken}
+            >
+              {submitted ? "Sent" : "Send"}
+            </button>
+          </div>
         </div>
       </div>
     </SectionWrapper>
