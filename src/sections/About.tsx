1fccf5ea8f0c614ec13c349a91cbf5a0b52b8600
seifmegahed <seifmegahed@me.com>
2024-06-23 23:57:18 +0300

    skill bar + animation on enter

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef, useState } from "react";
 import SectionTitle from "../components/SectionTitle";
 import SectionWrapper from "../components/SectionWrapper";
 import { aboutData, skillsData } from "../data/aboutData";
@@ -7,6 +8,26 @@ const ParagraphTitle = (props: { children: string }) => (
 );
 
 export default function About() {
+  const skillsSectionRef = useRef<HTMLDivElement>(null);
+  const [trigger, setTrigger] = useState(false);
+
+  useEffect(() => {
+    if (!trigger) {
+      const interval = setInterval(() => {
+        const skillsSection = skillsSectionRef.current;
+        if (skillsSection) {
+          const skillsSectionHeight =
+            window.innerHeight - skillsSection.getBoundingClientRect().top;
+          if (skillsSectionHeight > 300) {
+            setTrigger(true);
+            console.log("trigger");
+            clearInterval(interval);
+          }
+        }
+      }, 100);
+    }
+  }, [trigger]);
+
   const PartA = [
     aboutData[0],
     aboutData[1],
@@ -35,7 +56,7 @@ export default function About() {
               <p>{data.content}</p>
             </div>
           ))}
-          <div>
+          <div ref={skillsSectionRef}>
             <ParagraphTitle>Skills</ParagraphTitle>
             <div className="grid lg:grid-cols-2  md:grid-cols-1 sm:grid-cols-2 gap-4 h-full">
               {Object.entries(skillsData).map(([key, value]) => (
@@ -45,10 +66,10 @@ export default function About() {
                   {value.map((data) => (
                     <div
                       key={data.title}
-                      className="flex w-full justify-between"
+                      className="flex w-full justify-between items-center"
                     >
                       <p>{data.title}</p>
-                      <p>{data.score}</p>
+                      <SkillBar trigger={trigger} score={data.score} />
                     </div>
                   ))}
                 </div>
@@ -58,5 +79,16 @@ export default function About() {
         </div>
       </div>
     </SectionWrapper>
+  );
+}
+
+function SkillBar(props: { score: number; trigger: boolean }) {
+  return (
+    <div className="bg-white rounded-full h-2 w-24">
+      <div
+        className="bg-slate-500 h-2 rounded-full transition-all duration-1000 ease-in-out"
+        style={{ width: props.trigger ? props.score + "%" : "0%" }}
+      ></div>
+    </div>
   );
 }
