e5f3d67cada4778ec106ea294e2c472a0d3d79d0
seifmegahed <seifmegahed@me.com>
2024-06-23 23:19:14 +0300

    skills section

---
+++ src/data/aboutData.ts
@@ -0,0 +1,77 @@
+export const aboutData = [
+  {
+    span: "1",
+    title: "My Passion for Automating Electronics with Software",
+    content:
+      "I've always been captivated by electronics and their potential to create intelligent systems. Throughout my career, I've combined my electronics expertise with software development to bring projects to life.",
+  },
+  {
+    span: "1",
+    title: "From Hardware to Streamlined Processes",
+    content:
+      "My experience spans various electronic projects, designing circuits and PCBs. I've also developed software using C and C++ for these projects.",
+  },
+  {
+    span: "2",
+    title: "Building an Automated Solution",
+    content:
+      "I spearheaded a transformation, implementing a system that leveraged a computerized database and automated software for ordering and tracking components. This Google Sheets-based solution significantly reduced purchasing time and effort while boosting accuracy and efficiency.",
+  },
+  {
+    span: "2",
+    title: "A Journey into Web Development",
+    content:
+      "As the system evolved, I recognized the need for scripting to enhance its complexity. This sparked my interest in web development. Starting with Google Apps Script, I automated tasks and built front-end interfaces using HTML and CSS. Additionally, I convinced the accounting department to transition from Excel to Google Sheets.",
+  },
+  {
+    span: "2",
+    title: "A Centralized and Scalable System",
+    content:
+      "I built a centralized accounting system using Google Sheets, ensuring a single source of truth for financial data. It offered user-friendly forms for data entry and reporting, replacing the previous system of shared Excel files. This new system was modular and scalable, allowing for future integrations and feature additions.",
+  },
+  {
+    span: "2",
+    title: "Discovering a New Passion",
+    content:
+      "This experience ignited a passion for web development. Recognizing the limitations of Apps Script, I delved into real web development. After researching popular libraries, I learned React through an online course on Scrimba. Since then, I've actively applied my React skills in multiple projects.",
+  },
+  {
+    span: "2",
+    title: "TL;DR",
+    content:
+      "Electronics engineer + software developer. Built automated systems using electronics and code (C/C++). Streamlined a company's purchasing process with a custom database and web interface (Google Sheets, Apps Script). Learned React for web development and applied it to new projects. Now passionate about using software",
+  },
+];
+
+export const skillsData = {
+  Software: [
+    { title: "C", score: 70 },
+    { title: "C++", score: 50 },
+    { title: "Java", score: 50 },
+    { title: "TypeScript", score: 80 },
+    { title: "React", score: 80 },
+    { title: "Tailwind CSS", score: 80 },
+    { title: "React Query", score: 70 },
+    { title: "PostgreSQL", score: 50 },
+    { title: "OOP", score: 40 },
+    { title: "FP", score: 90 },
+    { title: "ThreeJS", score: 50 },
+  ],
+  Hardware: [
+    { title: "EMC", score: 80 },
+    { title: "Wireless", score: 60 },
+    { title: "Mixed Signal", score: 80 },
+    { title: "Analog", score: 80 },
+    { title: "PLC", score: 80 },
+    { title: "Low Power", score: 70 },
+    { title: "Control Systems", score: 70 },
+    { title: "FPGA", score: 30 },
+  ],
+  Design: [
+    { title: "Eagle", score: 100 },
+    { title: "Fusion 360", score: 80 },
+    { title: "Solidworks", score: 80 },
+    { title: "Blender", score: 30 },
+    { title: "Figma", score: 50 },
+  ],
+};
--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,98 +1,60 @@
 import SectionTitle from "../components/SectionTitle";
 import SectionWrapper from "../components/SectionWrapper";
+import { aboutData, skillsData } from "../data/aboutData";
 
 const ParagraphTitle = (props: { children: string }) => (
   <p className="text-lg w-full font-bold">{props.children}</p>
 );
 
 export default function About() {
+  const PartA = [
+    aboutData[0],
+    aboutData[1],
+    aboutData[2],
+    aboutData[3],
+    aboutData[4],
+    aboutData[5],
+  ];
+  const PartB = [aboutData[6]];
   return (
     <SectionWrapper id="about">
       <SectionTitle title="About" />
-      <div className="grid md:grid-cols-2 gap-5">
-        <div className="flex flex-col gap-5 row-span-3">
-          <div>
-            <ParagraphTitle>
-              My Passion for Automating Electronics with Software
-            </ParagraphTitle>
-            <p>
-              I've always been captivated by electronics and their potential to
-              create intelligent systems. Throughout my career, I've combined my
-              electronics expertise with software development to bring projects
-              to life.
-            </p>
-          </div>
-          <div>
-            <ParagraphTitle>
-              From Hardware to Streamlined Processes
-            </ParagraphTitle>
-            <p>
-              My experience spans various electronic projects, working with
-              circuits and components. I've also developed software using C and
-              C++ for these projects.
-            </p>
-            <p>
-              In a previous role at a company specializing in industrial
-              electrical systems, I managed the Purchasing and Planning
-              department. Initially, the process was paper-based, decentralized,
-              and inefficient.
-            </p>
-          </div>
-          <div>
-            <ParagraphTitle>Building an Automated Solution</ParagraphTitle>
-            <p>
-              I spearheaded a transformation, implementing a system that
-              leveraged a computerized database and automated software for
-              ordering and tracking components. This Google Sheets-based
-              solution significantly reduced purchasing time and effort while
-              boosting accuracy and efficiency.
-            </p>
-          </div>
-          <div>
-            <ParagraphTitle>A Journey into Web Development</ParagraphTitle>
-            <p>
-              As the system evolved, I recognized the need for scripting to
-              enhance its complexity. This sparked my interest in web
-              development. Starting with Google Apps Script, I automated tasks
-              and built front-end interfaces using HTML and CSS. Additionally, I
-              convinced the accounting department to transition from Excel to
-              Google Sheets.
-            </p>
-          </div>
-          <div>
-            <ParagraphTitle>A Centralized and Scalable System</ParagraphTitle>
-            <p>
-              I built a centralized accounting system using Google Sheets,
-              ensuring a single source of truth for financial data. It offered
-              user-friendly forms for data entry and reporting, replacing the
-              previous system of shared Excel files. This new system was modular
-              and scalable, allowing for future integrations and feature
-              additions.
-            </p>
-          </div>
-          <div>
-            <ParagraphTitle>Discovering a New Passion</ParagraphTitle>
-            <p>
-              This experience ignited a passion for web development. Recognizing
-              the limitations of Apps Script, I delved into real web
-              development. After researching popular libraries, I learned React
-              through an online course on Scrimba. Since then, I've actively
-              applied my React skills in multiple projects.
-            </p>
-          </div>
+      <div className="grid md:grid-cols-2 md:gap-5 gap-2 w-full">
+        <div className="flex flex-col gap-3">
+          {PartA.map((data) => (
+            <div key={data.title}>
+              <ParagraphTitle>{data.title}</ParagraphTitle>
+              <p>{data.content}</p>
+            </div>
+          ))}
         </div>
-        <div className="grid-rows-1">
-          <ParagraphTitle>TL;DR</ParagraphTitle>
-          <p>
-            Electronics engineer + software developer. Built automated systems
-            using electronics and code (C/C++). Streamlined a company's
-            purchasing process with a custom database and web interface (Google
-            Sheets, Apps Script). Learned React for web development and applied
-            it to new projects. Now passionate about using software
-          </p>
-        </div>
-        <div>
-          <ParagraphTitle>Skills</ParagraphTitle>
+        <div className="flex flex-col gap-2">
+          {PartB.map((data) => (
+            <div key={data.title}>
+              <ParagraphTitle>{data.title}</ParagraphTitle>
+              <p>{data.content}</p>
+            </div>
+          ))}
+          <div>
+            <ParagraphTitle>Skills</ParagraphTitle>
+            <div className="grid lg:grid-cols-2  md:grid-cols-1 sm:grid-cols-2 gap-4 h-full">
+              {Object.entries(skillsData).map(([key, value]) => (
+                <div key={key}>
+                  <p>{key}</p>
+                  <hr className="border-gray-400"></hr>
+                  {value.map((data) => (
+                    <div
+                      key={data.title}
+                      className="flex w-full justify-between"
+                    >
+                      <p>{data.title}</p>
+                      <p>{data.score}</p>
+                    </div>
+                  ))}
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
     </SectionWrapper>
