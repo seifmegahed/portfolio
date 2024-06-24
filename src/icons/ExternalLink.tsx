1dd2b0964bbfafc0811fb51f488886a56ceba8ac
seifmegahed <seifmegahed@me.com>
2024-06-25 01:31:16 +0300

    External Link Icon

---
+++ src/icons/ExternalLink.tsx
@@ -0,0 +1,22 @@
+export default function ExternalLink(props: { href?: string }) {
+  return (
+    <div className="scale-90 cursor-pointer hover:scale-100 transition-all duration-300 ease-in-out">
+      <a href={props.href} target="_blank" rel="noreferrer">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="size-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+          />
+        </svg>
+      </a>
+    </div>
+  );
+}
