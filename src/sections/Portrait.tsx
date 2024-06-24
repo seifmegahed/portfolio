154c2afa937220558519cf74fc4527a3463e7d34
seifmegahed <seifmegahed@me.com>
2024-06-25 01:30:49 +0300

    photo externally hosted

--- src/sections/Portrait.tsx
+++ src/sections/Portrait.tsx
@@ -1,9 +1,9 @@
-import portraitImage from "../assets/portrait.jpg";
+const portraitImageURL = "https://utfs.io/f/b3e3a10e-2085-429c-881d-2d1a6b04191f-oj13b9.jpg";
 
 export default function Portrait() {
   return (
     <div className="w-full h-full flex flex-col justify-center items-end">
-      <img className="rounded-r-full w-60" src={portraitImage} />
+      <img className="rounded-r-full w-60" src={portraitImageURL} />
     </div>
   );
 }
