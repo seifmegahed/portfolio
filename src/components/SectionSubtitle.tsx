da2af091ba4741f348ebc30bcaf47a95a5effdf3
seifmegahed <seifmegahed@me.com>
2024-06-25 02:55:00 +0300

    styling fix

--- src/components/SectionSubtitle.tsx
+++ src/components/SectionSubtitle.tsx
@@ -1,7 +1,7 @@
 export default function SectionSubtitle({ subtitle }: { subtitle: string }) {
   return (
-    <div className="text-xl font-normal flex gap-1">
-      <p>{subtitle}</p>
+    <div className="w-full">
+      <h1 className="text-xl mb-5 text-slate-300">{subtitle}</h1>
     </div>
   );
 }
