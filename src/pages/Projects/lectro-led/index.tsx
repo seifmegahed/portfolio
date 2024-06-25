1cb9d01ce27a08d65dd0049a6c001c2ec67c954d
seifmegahed <seifmegahed@me.com>
2024-06-25 19:28:54 +0300

    init lectro-led page

--- src/pages/Projects/lectro-led/index.tsx
+++ src/pages/Projects/lectro-led/index.tsx
@@ -1,5 +1,6 @@
 import Navbar from "../../../components/Navbar";
 import PageWrapper from "../../../components/PageWrapper";
+import SectionSubtitle from "../../../components/SectionSubtitle";
 import SectionTitle from "../../../components/SectionTitle";
 import SectionWrapper from "../../../components/SectionWrapper";
 
@@ -8,11 +9,15 @@ export default function LectroLED() {
     <PageWrapper navbar={<Navbar />}>
       <SectionWrapper id="/lectro-led">
         <SectionTitle title="Lectro LED" />
+        <SectionSubtitle subtitle="Web app to manage an LED Lighting manufacturer's resources" />
         <div className="w-full h-full flex items-center flex-col max-w-screen-lg justify-center text-center gap-5">
-          <p>Web app to manage an LED Lighting manufacturer's resources</p>
           <p>Coming Soon!</p>
         </div>
       </SectionWrapper>
     </PageWrapper>
   );
 }
+
+/*
+
+*/
