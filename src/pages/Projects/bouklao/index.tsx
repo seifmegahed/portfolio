db4c5b5038882d03da2f53f0936a07957163aba7
seifmegahed <seifmegahed@me.com>
2024-07-21 19:45:14 +0300

    added bouklao images

--- src/pages/Projects/bouklao/index.tsx
+++ src/pages/Projects/bouklao/index.tsx
@@ -4,6 +4,10 @@ import PageWrapper from "@/components/PageWrapper";
 import SectionTitle from "@/components/SectionTitle";
 import SectionWrapper from "@/components/SectionWrapper";
 
+const iphoneImageURL = "https://utfs.io/f/b763a7be-406f-4a22-a7cd-6fc369884d6d-ab5o5j.png"
+const ipadImageURL = "https://utfs.io/f/a232a8a9-33ed-49b7-9ca4-a2c17904a27b-1czhm.png"
+const ipadImageURL2 = "https://utfs.io/f/918a10ca-53fc-4f41-bd44-40ab076081c5-sxnfmz.png"
+
 export default function Bouklao() {
   return (
     <PageWrapper navbar={<Navbar />}>
@@ -20,6 +24,9 @@ export default function Bouklao() {
             <ExternalLink href="https://bouklao.vercel.app/" />
           </div>
           <p>Coming Soon!</p>
+          <img src={iphoneImageURL} />
+          <img src={ipadImageURL} />
+          <img src={ipadImageURL2} />
         </div>
       </SectionWrapper>
     </PageWrapper>
